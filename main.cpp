61dc69e40b51f191ceebaf0ae2514c8be83da79d
Vale088 <valerio.sinni@marconibari.edu.it>
2024-11-07 09:07:02 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,23 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+   int N,numDivisori;
+   float quoziente,resto,d,c;
+   cout<<"scrivi un numero intero"<<endl;
+   cin>>N;
+   numDivisori=0;
+   for(d=1;d<=N;d++){
+    quoziente=N/d;
+    resto=N/d;
+    if(resto=0){
+        cout<<d & "è divisore"<<endl; 
+        numDivisori=numDivisori+1;
+    }
+     if(numDivisori=2){
+        cout<<N &"è primo"<<endl;
+     }else{ 
+        cout<<N & "non è un numero primo"<<endl; 
+     }
+   }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
